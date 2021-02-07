389d722efbfe043452332b95068314b105e8c35c
Phan Quoc Bao <baopqse160018@fpt.edu.vn>
2021-02-07 15:47:36 +0700

    fix bug show agian todo deleted & responsive loading page + add delete button

--- todosite/app.js
+++ todosite/app.js
@@ -1,55 +1,84 @@
-
 // Selectors
 let checkBoxs = document.querySelectorAll('.todo__check input[type="checkbox"]'),
       todoInput = document.querySelector('.todo__input'),
       todoList = document.querySelector('.todo__list'),
       todoButton = document.getElementById('todo__btn'),
       html= '',
-      count;
+      count,
+      todoTrash = document.querySelector('.todo__trash');
 
+      
 // Event Listener
+
+todoButton.onclick = addInput;
+
+// Excute when users click checked button
 todoList.onclick = (e) => {
   var check = e.target.id.trim() ==='';
+  let contentOfTodo = e.target.parentElement.parentElement.parentElement.innerText,
+      elementCheckbox = e.target.nextElementSibling,
+      elementTodo = e.target.parentElement.parentElement.parentElement,
+      elementTodoContent = e.target.parentElement.parentElement.parentElement.querySelector('.todo__content');
   if (!check){
-    let todos;
-    let saveCount;
-    let todoContent = e.target.parentElement.parentElement.parentElement.innerText;
-    let todoCheckbox = e.target.nextElementSibling;
-    let todoItem = e.target.parentElement.parentElement.parentElement;
-    let todoChecked = e.target.parentElement.parentElement.parentElement.querySelector('.todo__content');
 
-    todoChecked.classList.toggle('checked');
-    todoItem.classList.add('done');
-    todoCheckbox.addEventListener('transitionend',()=>{
-        todoItem.remove();
+    elementTodoContent.classList.toggle('checked');
+    elementTodo.classList.add('done');
+    elementCheckbox.addEventListener('transitionend',()=>{
+        elementTodo.remove();
     });
-    removeSaveLocalStorage(todoContent); 
+    removeSaveLocalStorage(contentOfTodo); 
   }
+ if (e.target.className === 'todo__trash'){
+     e.target.parentElement.classList.add('delete');
+        e.target.parentElement.remove();
+     removeSaveLocalStorage(e.target.parentElement.querySelector('.todo__content').innerText);
+    
+ }
 }
 
- todoButton.onclick = addInput;
+//  Excute when press Enter
  window.onkeyup = (e) => {
    if (e.keyCode === 13) {
      addInput();
    }
  }
+
+//  Excute onload
 document.addEventListener('DOMContentLoaded',()=>{
-    loadingPage();
-    getTodos();
+    renderTodos();
     count = checkCount();
 });
 
+// Excute loadingPage Function first of all;
+loadingPage();
+
+// Delete todo;
+
+
 // Functions
-function renderTodos (value, id) {
-    return `<li class="todo__item">
-    <div class="checkbox__container">
-      <label class="todo__check" for="todo__toggle${id}">
-  <input type="checkbox" id="todo__toggle${id}">
-  <div class="checkbox"></div>
-  </label></div>
-      <p class="todo__content">${value}</p>
-  </li>`;
+
+function renderTodos () {
+    let todos;
+    let html = '';
+    if (localStorage.getItem('todos') === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach((todo, index) => {
+        html += `<li class="todo__item">
+        <div class="checkbox__container">
+          <label class="todo__check" for="todo__toggle${index+1}">
+      <input type="checkbox" id="todo__toggle${index+1}">
+      <div class="checkbox"></div>
+      </label></div>
+          <p class="todo__content">${todo}</p>
+          <a class="todo__trash"><i class="fa fa-trash"></i></a>
+      </li>`;
+    }); 
+    todoList.innerHTML = html;
 }
+
 function checkCount() {
     let saveCount;
     if (localStorage.getItem('todos') === null){
@@ -61,12 +90,12 @@ function checkCount() {
     }
     return saveCount;
 }
+
 function addInput() {
   if ((todoInput.value.trim() !='')){
   count++;
-  html += renderTodos(todoInput.value, count);
-  todoList.innerHTML = html;
   saveLocalTodos(todoInput.value);
+  renderTodos ();
   todoInput.value = '';
  
   } else alert('You need enter a value'); 
@@ -76,7 +105,7 @@ function saveLocalTodos(todo){
     //Check -- Do i already have things in there?
     
     let todos;
-    let saveCount
+    let saveCount;
     if (localStorage.getItem('todos') === null){
         todos = [];
         saveCount = 0;
@@ -87,20 +116,8 @@ function saveLocalTodos(todo){
     localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function getTodos (){
-    let todos;
-    if (localStorage.getItem('todos') === null){
-        todos = [];
-    } else {
-        todos = JSON.parse(localStorage.getItem('todos'));
-    }
-    todos.forEach((todo, index) => {
-        html += renderTodos(todo, index+1);
-    });
-    todoList.innerHTML = html;
-}
 
-function removeSaveLocalStorage (todoIndex) {
+function removeSaveLocalStorage (todo) {
     let todos;
     if (localStorage.getItem('todos') === null){
         todos = [];
@@ -115,13 +132,14 @@ function removeSaveLocalStorage (todoIndex) {
 }
 
 function loadingPage (){
-    setTimeout(
-        () => {
+    setTimeout(() => {
             const loading = document.querySelector('.loading-page');
             loading.style.opacity = "0";
             loading.addEventListener ('transitionend', () => {
                 loading.style.display = "none";
+                loading.remove();
             })
-        }
-        ,3000)
+            
+                }
+            ,3000)
 }
